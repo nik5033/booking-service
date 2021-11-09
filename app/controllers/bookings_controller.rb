9df3203b0cdd44efd2685392ec7ab75dac0174cc
Nikita Gudkov <gudkov.nikita@bk.ru>
2021-11-10 00:04:53 +0300

    Ticket_project remove unnecessary methods from Booking model controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,22 +1,8 @@
 class BookingsController < ApplicationController
   before_action :set_booking, only: %i[ show edit update destroy ]
 
-  # GET /bookings or /bookings.json
-  def index
-    @bookings = Booking.all
-  end
-
   # GET /bookings/1 or /bookings/1.json
   def show
-  end
-
-  # GET /bookings/new
-  def new
-    @booking = Booking.new
-  end
-
-  # GET /bookings/1/edit
-  def edit
   end
 
   # POST /bookings or /bookings.json
@@ -44,15 +30,6 @@ class BookingsController < ApplicationController
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @booking.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # DELETE /bookings/1 or /bookings/1.json
-  def destroy
-    @booking.destroy
-    respond_to do |format|
-      format.html { redirect_to bookings_url, notice: "Booking was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
