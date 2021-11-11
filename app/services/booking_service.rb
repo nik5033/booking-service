cd1aa3bfbb9dc180cc4a67189b28918ad73bcb5a
Nikita Gudkov <gudkov.nikita@bk.ru>
2021-11-11 23:28:20 +0300

    Ticket_project fix booking service

--- app/services/booking_service.rb
+++ app/services/booking_service.rb
@@ -8,7 +8,7 @@ class BookingService
   def valid?
     booking = Booking.find(params[:id])
 
-    if Time.now - booking[:created_at].min < 5*60 and booking[:status] == 1
+    if Time.now - booking[:created_at] < 5*60 and booking[:status] == 1
       return true 
     else
       booking.update(status: 0)
